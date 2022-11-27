03403910241006e6653151d0a1f22abe0c70ee31
Juan Sebastian Alvarez Eraso <js.alvareze@uniandes.edu.co>
2022-11-26 23:26:11 -0500

    Se usan los modelos entrenados con KMEANS

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import geopandas as gpd
+import joblib
+import pandas as pd
 from flask import Flask
 from flask import jsonify
+from flask import request
 
 from config import config
 
@@ -12,6 +16,13 @@ def create_app(arg_environment):
 
 environment = config['development']
 app = create_app(environment)
+gdf = gpd.read_file('data/Catastro_gdb.geojson')
+
+hurtos_groups = pd.read_csv('data/medellin_hurtos_groups.csv')
+features = ['hurtos_peligrosos', 'hurtos_no_peligrosos', 'hurtos_a_mujeres', 'hurtos_a_hombres',
+            'hurtos_en_transporte_publico', 'hurtos_en_transporte_particular', 'hurtos_en_via_publica', 'hurtos_niños',
+            'hurtos_jovenes', 'hurtos_adultos', 'hurtos_adultos_mayores', 'hurtos']
+X = hurtos_groups[features]
 
 
 @app.route('/ping', methods=['GET'])
@@ -21,8 +32,28 @@ def get_ping():
 
 @app.route('/predict', methods=['POST'])
 def post_predict():
-    return jsonify('Una predicción ...')
+    args = request.args
+    args = args.to_dict()
+    clusters_quantity = args.get('clusters_quantity')
+
+    model = joblib.load('models/km' + clusters_quantity + '.joblib')
+    results = model.predict(X.values)
+    results = pd.DataFrame([X.index, results]).T
+
+    df_temp = hurtos_groups.join(results)
+    clustered_df = pd.DataFrame()
+    clustered_df['cluster'] = df_temp[1]
+    clustered_df['codigo_barrio'] = df_temp['codigo_barrio']
+
+    barrios = []
+    for index, row in clustered_df.iterrows():
+        barrios.append({
+            'barrio': row['codigo_barrio'],
+            'cluster': row['cluster']
+        })
+
+    return jsonify({'barrios': barrios})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug = False)
+    app.run(host='0.0.0.0', debug=False)
